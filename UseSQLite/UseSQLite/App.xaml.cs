0cd877119fb6e7cb3e0ca8394cf47bc5d1729eb0
naoyuki <nfrutta1208@gmail.com>
2021-03-05 17:30:10 +0900

    [UseSQLite]add

--- UseSQLite/UseSQLite/App.xaml.cs
+++ UseSQLite/UseSQLite/App.xaml.cs
@@ -1,28 +1,47 @@
 ﻿using System;
+using System.IO;
+using UseSQLite.Data;
+using UseSQLite.Views;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace UseSQLite
 {
     public partial class App : Application
     {
+        static NoteDatabase database;
+
+        public static NoteDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new NoteDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Notes.db3"));
+                }
+                return database;
+            }
+        }
+
+        // Xamarin.Forms のエントリーポイント
         public App()
         {
             InitializeComponent();
-
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new NotesPage());
         }
 
         protected override void OnStart()
         {
+            // Handle when your app starts
         }
 
         protected override void OnSleep()
         {
+            // Handle when your app sleeps
         }
 
         protected override void OnResume()
         {
+            // Handle when your app resumes
         }
     }
 }
