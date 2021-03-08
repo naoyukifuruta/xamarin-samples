d492bf07e66763da635939a00e89a1fae73ceeee
naoyuki <nfrutta1208@gmail.com>
2021-03-08 12:11:25 +0900

    [UsePrism]first commit.

---
+++ UsePrism/UsePrism/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Prism;
+using Prism.Ioc;
+using UsePrism.ViewModels;
+using UsePrism.Views;
+using Xamarin.Forms;
+
+namespace UsePrism
+{
+    public partial class App
+    {
+        public App() : this(null) { }
+
+        public App(IPlatformInitializer platformInitializer) : base(platformInitializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+            NavigationService.NavigateAsync("NavigationPage/MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+        }
+    }
+}
---
+++ UsePrism/UsePrism/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace UsePrism.ViewModels
+{
+    public class MainPageViewModel
+    {
+        public MainPageViewModel()
+        {
+        }
+    }
+}
