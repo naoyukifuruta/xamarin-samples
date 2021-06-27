2839b72abb8c48442eecae0ee26de1427d5128b5
naoyuki <nfrutta1208@gmail.com>
2021-06-27 22:52:50 +0900

    サイト見ながらちょっと使ってみた

---
+++ UsePopupPagePlugin/UsePopupPagePlugin/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rg.Plugins.Popup.Services;
+using Xamarin.Forms;
+
+namespace UsePopupPagePlugin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void ClickPopupShowButton(object o, EventArgs e)
+        {
+            var page = new PopupTestPage();
+            await PopupNavigation.Instance.PushAsync(page);
+            await Task.Delay(5000);
+            await PopupNavigation.Instance.PopAsync();
+        }
+    }
+}
---
+++ UsePopupPagePlugin/UsePopupPagePlugin/PopupTestPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Rg.Plugins.Popup.Pages;
+using Xamarin.Forms;
+
+namespace UsePopupPagePlugin
+{
+    public partial class PopupTestPage : PopupPage
+    {
+        public PopupTestPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
